8a682c60cf4ceb52c617fdcf2be01a97a5e49d8c
oliviahe0111 <oliviahe0111@gmail.com>
2024-05-07 00:02:52 -0700

    all files added

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Post from './Post';
+
+function App() {
+  return (
+    <main> 
+      <header>
+        <a href="" className='logo'>MyBlog</a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
+      </header>
+      <Post/>
+      <Post/>
+      <Post/>
+
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post(){
+    return(
+        <div className='post'>
+            <div className='image'>
+            <img src='https://images.lifestyleasia.com/wp-content/uploads/sites/6/2024/01/31141627/south-korean-singer-actress-iu-boyfriend-personal-life-lee-jong-suk-bts-v.jpg'/>
+            </div>
+            <div className="texts">
+            <h2>Post headline</h2>
+            <p className='info'>
+                <a className='author'>Olivia He</a>
+                <time>2024-05-06</time>
+            </p>
+            <p className='summary'>Discover a selection of our favourite bakes. Whether you fancy making bread, brownies, cookies, cakes or tarts, we've got the perfect recipe for every occasion.</p>
+            </div>
+        </div>
+    );
+}
+
