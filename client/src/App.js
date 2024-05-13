e58a746c8c57438a21ebf7e238f73f7ebcc35b66
oliviahe0111 <oliviahe0111@gmail.com>
2024-05-13 12:58:54 -0700

    login and register page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Post from './Post';
 
+
 function App() {
   return (
-    <main> 
-      <Header></Header>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-
-
-    </main>
+    <Routes>
+      <Route index element={
+        <main> 
+          <Header></Header>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+        </main>
+      } />
+      <Route path={'/login'} element={
+        <main>
+          <Header />
+          <div>login page</div>
+        </main>
+      } />
+      <Route path={'/register'} element={
+        <div>register page</div>
+      } />
+    </Routes>
+      
   );
 }
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 
 export default function Header(){
     return(
         <header>
         <a href="" className='logo'>MyBlog</a>
         <nav>
-        <a href=''>Login</a>
-        <a href=''>Register</a>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
         </nav>
         </header>
     );
