b38a265e62aa6d8bf81f3a71a58b004796635512
oliviahe0111 <oliviahe0111@gmail.com>
2024-05-13 15:32:25 -0700

    login and register page design set up donw

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,26 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Post from './Post';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/Loginpage';
+import RegisterPage from './pages/RegisterPage';
 
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-        <main> 
-          <Header></Header>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
-        </main>
-      } />
-      <Route path={'/login'} element={
-        <main>
-          <Header />
-          <div>login page</div>
-        </main>
-      } />
-      <Route path={'/register'} element={
-        <div>register page</div>
-      } />
+      <Route path="/" element={<Layout/>}>
+        <Route path={'/'}index element={
+          <IndexPage/>
+        } />
+        <Route path={'/login'} element={
+          <LoginPage/>
+        } />
+        <Route path={'/register'} element={
+          <RegisterPage/>
+        } />
+      </Route>
     </Routes>
       
   );
