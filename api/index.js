6aa0ea3bc18bb89a565c3e1bbd4c60a51f894a04
oliviahe0111 <oliviahe0111@gmail.com>
2024-05-15 17:13:48 -0700

    authentication done, navigate back to main page after successful login

--- api/index.js
+++ api/index.js
@@ -7,8 +7,9 @@ const app = express();
 const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = "djfsdbalhgdjlshfg"
 
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://oliviahe0111:GI85DK5XgvdgJLId@cluster0.wsfnpkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -32,9 +33,12 @@ app.post('/login', async (req,res) =>{
     const {username, password} = req.body;
     const userDoc =await User.findOne({username}); //find user document in DB
     const passwordOk = bcrypt.compareSync(password, userDoc.password);
-    res.json(passwordOk);
     if(passwordOk){
         //logged in
+        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token)=>{
+            if(err) throw err;
+            res.cookie('token', token).json('ok');
+        });
     } else{
         res.status(400).json("wrong credentials")
     }
